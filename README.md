f815052c968c4eabd80fc338bf986e49094fd366
Harshal Bhatia~ <2299-harshal.bhatia@users.noreply.source.golabs.io>
2020-12-27 19:00:47 +0530

    Add zsh plugin installation

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Dotfiles
 
 ### TODO
-[x] - move bin
-[x] - automate linking
-[x] - link bin
-[x] - install oh-my-zsh https://github.com/sapegin/dotfiles/commit/d13377563b77eaaa80ee7656cc4ccb1ece905d4b
-[ ] - ssh keygen
-[x] - install powerline fonts
-[ ] - brewfile setup
-[ ] - sync iterm settings
-[ ] - mackup?
-[ ] - global ignore .history
+ - [x] move bin
+ - [x] automate linking
+ - [x] link bin
+ - [x] install oh-my-zsh https://github.com/sapegin/dotfiles/commit/d13377563b77eaaa80ee7656cc4ccb1ece905d4b
+ - [ ] ssh keygen
+ - [x] install powerline fonts
+ - [ ] brewfile setup
+ - [ ] sync iterm settings
+ - [ ] mackup?
+ - [ ] global ignore .history
+ - [ ] n vs nvm for perf: https://blog.mattclemente.com/2020/06/26/oh-my-zsh-slow-to-load.html
+ 
--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -145,6 +145,12 @@ install_oh_my_zsh() {
   else 
       echo "$FILE does not exist. Installing"
       git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+      # Plugins
+      git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+      git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+      git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+      git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+      git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
   fi
 }
 
