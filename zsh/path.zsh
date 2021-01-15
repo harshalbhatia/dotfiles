412f1fb8e4d3808d2109045488dba6370f49f294
Harshal Bhatia <harshalbhatia@users.noreply.github.com>
2021-01-16 01:22:33 +0530

    Use default PATH instead of overwriting

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,13 +9,15 @@ function _prepend_path() {
 # 1. Default paths
 # 2. ./node_modules/.bin - shorcut to run locally installed Node bins
 # 3. Custom bin folder for n, Ruby, CoreUtils, dotfiles, etc.
-PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
-[ -d /usr/local/bin ] && _prepend_path "/usr/local/bin"
+
+# To use default PATH instead of overwriting, comment below line.
+# PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
+# [ -d /usr/local/bin ] && _prepend_path "/usr/local/bin"
 [ -d /usr/local/opt/curl/bin ] && _prepend_path "/usr/local/opt/curl/bin"
 [ -d /usr/local/sbin ] && _prepend_path "/usr/local/sbin"
 [ -d /usr/local/opt/ruby/bin ] && _prepend_path "/usr/local/opt/ruby/bin"
 [ -d /usr/local/opt/coreutils/libexec/gnubin ] && _prepend_path "/usr/local/opt/coreutils/libexec/gnubin"
-[ -d $GOPATH/bin:$PATH ] && _prepend_path "$GOPATH/bin:$PATH"
+[ -d $GOPATH/bin ] && _prepend_path "$GOPATH/bin:$PATH"
 [ -d ~/dotfiles/bin ] && _prepend_path "$HOME/dotfiles/bin"
-[ -d ${KREW_ROOT:-$HOME/.krew}/bin:$PATH ] && _prepend_path "${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+[ -d ${KREW_ROOT:-$HOME/.krew}/bin ] && _prepend_path "${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 export PATH
