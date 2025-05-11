6a2fb8fbda368e4ac3dfa3789ac6493a4884bdb1
Harshal Bhatia <harshalbhatia@users.noreply.github.com>
2025-05-11 08:24:33 -0400

    Refactor(zsh): Streamline path.zsh by removing unused Krew, PNPM config and simplifying cargo path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,9 +1,9 @@
 # Prepend $PATH without duplicates
-# function _prepend_path() {
-# 	if ! $( echo "$PATH" | tr ":" "\n" | grep -qx "$1" ) ; then
-# 		PATH="$1:$PATH"
-# 	fi
-# }
+function _prepend_path() {
+	if [[ ":${PATH}:" != *":$1:"* ]]; then
+		PATH="$1${PATH:+:${PATH}}"
+	fi
+}
 
 # Construct $PATH
 # 1. Default paths
@@ -11,13 +11,21 @@
 # 3. Custom bin folder for n, Ruby, CoreUtils, dotfiles, etc.
 
 # To use default PATH instead of overwriting, comment below line.
-# PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
-# [ -d /usr/local/bin ] && _prepend_path "/usr/local/bin"
-# [ -d /usr/local/opt/curl/bin ] && _prepend_path "/usr/local/opt/curl/bin"
-# [ -d /usr/local/sbin ] && _prepend_path "/usr/local/sbin"
-# [ -d /usr/local/opt/ruby/bin ] && _prepend_path "/usr/local/opt/ruby/bin"
-# [ -d /usr/local/opt/coreutils/libexec/gnubin ] && _prepend_path "/usr/local/opt/coreutils/libexec/gnubin"
-# [ -d $GOPATH/bin ] && _prepend_path "$GOPATH/bin:$PATH"
-# [ -d ~/dotfiles/bin ] && _prepend_path "$HOME/dotfiles/bin"
-# [ -d ${KREW_ROOT:-$HOME/.krew}/bin ] && _prepend_path "${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-# export PATH
+# PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin' # Initial base PATH, often handled by system
+
+# Homebrew specific paths
+_prepend_path "/opt/homebrew/bin" # General homebrew bin
+_prepend_path "/opt/homebrew/sbin" # General homebrew sbin
+_prepend_path "/opt/homebrew/opt/openssl@3/bin"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig"
+
+# User-specific paths
+_prepend_path "$HOME/go/bin"
+_prepend_path "$HOME/dotfiles/bin"
+_prepend_path "$HOME/.cargo/bin" # For Rust/Cargo, if used
+
+# Other common paths (check if still needed/installed)
+# _prepend_path "/usr/local/bin" # Already often in default PATH or covered by Homebrew
+# _prepend_path "/usr/local/sbin" # Already often in default PATH or covered by Homebrew
+
+export PATH
